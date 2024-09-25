ebdf18aeee7103cd7cbd0b1ebd74b7044e3b0592
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-09-25 11:56:51 +0300

    feat: fetch photos and ceated updated gallery

--- app.js
+++ app.js
@@ -4,4 +4,21 @@ const searchInput = document.querySelector(".search-input");
 const searchBtn = document.querySelector(".search-btn");
 let searchValue;
 
-function curatedPhotos() {}
+async function curatedPhotos() {
+  const dataFetch = await fetch("https://api.pexels.com/v1/curated?per_page=15&page=1", {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: auth,
+    },
+  });
+  const data = await dataFetch.json();
+  data.photos.forEach((photo) => {
+    const galleryImg = document.createElement("div");
+    galleryImg.classList.add("gallery-img");
+    galleryImg.innerHTML = `<img src=${photo.src.large}></img>
+    <p>${photo.photographer}</p>`;
+    gallery.appendChild(galleryImg);
+  });
+}
+curatedPhotos();
