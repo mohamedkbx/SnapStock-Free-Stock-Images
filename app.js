b7e510cef74a40349d371a99143413c2c6cb360a
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-09-25 13:43:27 +0300

    feat : add rest function and clear with edit on gallery-img: add download button

--- app.js
+++ app.js
@@ -33,8 +33,13 @@ function generatePictures(data) {
   data.photos.forEach((photo) => {
     const galleryImg = document.createElement("div");
     galleryImg.classList.add("gallery-img");
-    galleryImg.innerHTML = `<img src=${photo.src.large}></img>
-        <p>${photo.photographer}</p>`;
+    galleryImg.innerHTML = `
+        <div class="gallery-info">
+        <p>${photo.photographer}</p>
+        <a href=${photo.src.original}>Download</a>
+        </div>
+        <img src=${photo.src.large}></img>`;
+
     gallery.appendChild(galleryImg);
   });
 }
@@ -53,11 +58,16 @@ async function curatedPhotos() {
 }
 
 async function searchPhoto(query) {
+  clear();
   const data = await fetchApi(
     `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`
   );
-
   generatePictures(data);
+}
+
+function clear() {
+  gallery.innerHTML = "";
+  searchInput.value = "";
 }
 
 curatedPhotos();
