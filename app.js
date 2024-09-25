ce7740bba13121d59be8ce514d5f11874b122f1b
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-09-25 11:39:44 +0300

    feat : setup html and css

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-const API_KEY = "ZtgWwvbqcBHQzJawHDJ3gRszvsLXg3k3smARsiicN9J61pcJod5q5Ejf";
+const auth = "ZtgWwvbqcBHQzJawHDJ3gRszvsLXg3k3smARsiicN9J61pcJod5q5Ejf";
+const gallery = document.querySelector(".gallery");
+const searchInput = document.querySelector(".search-input");
+const searchBtn = document.querySelector(".search-btn");
+let searchValue;
+
+function curatedPhotos() {}
